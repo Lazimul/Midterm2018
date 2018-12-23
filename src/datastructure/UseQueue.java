d93c84ce67b6f488ee6613c9501a5102a58ceee8
lazimul <haque.lazimul@gmail.com>
2018-12-23 11:45:39 -0500

    initial submit

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +12,7 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<String>qu=new Queue<String>() {
 
 	}
 
